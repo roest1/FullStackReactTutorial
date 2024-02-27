ecd6815dc575c1b05e26f6a5e821b7cbdb170bc8
Riley Oest <95721769+roest1@users.noreply.github.com>
2024-02-26 22:01:49 -0600

    added login

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fullstackreacttutori56c796c5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
